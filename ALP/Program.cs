75831f851de3e25711f8384c34a2cf1a242b69f9
thelaughh <101090948+thelaughh@users.noreply.github.com>
2022-09-12 20:49:11 +0200

    XD

--- ALP/Program.cs
+++ ALP/Program.cs
@@ -1,12 +1,16 @@
 ﻿Console.WriteLine("Ahoj");
 Console.WriteLine("Kolik ti je?");
 int vek = Convert.ToInt32(Console.ReadLine());
-if (vek == 18)
+if (vek >= 18)
 {
-    Console.WriteLine("si plnoletý a můžeš mít s3x");
+    Console.WriteLine("si plnoletý a můžeš mít řidičák");
 }
-if (vek < 15)
+if (vek >= 15)
 {
-    Console.WriteLine("Mladej a bez sexu xddddd");
+    Console.WriteLine("dokončil si základku a máš občanku");
+}
+if(vek <= 15)
+{
+    Console.WriteLine("Jsi na základce a nemáš doklady plus jsi kid");
 }
 

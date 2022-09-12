d5c2f44abac336f59d0b47bbc00760b0936f8885
thelaughh <101090948+thelaughh@users.noreply.github.com>
2022-09-12 20:05:42 +0200

    Přidejte soubory projektu.

---
+++ ALP/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ahoj");
+Console.WriteLine("Kolik ti je?");
+int vek = Convert.ToInt32(Console.ReadLine());
+if (vek == 18)
+{
+    Console.WriteLine("si plnoletý a můžeš mít s3x");
+}
+if (vek < 15)
+{
+    Console.WriteLine("Mladej a bez sexu xddddd");
+}
+
